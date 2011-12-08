e2701746457db2ae50ae04b3fdf5824de3d8d7df
Steffen Mueller <smueller@cpan.org>
2011-12-08 08:22:49 +0100

    Add blurb about speeding up accessors conditionally

--- lib/Language/P/Object.pm
+++ lib/Language/P/Object.pm
@@ -7,6 +7,21 @@ use warnings;
 # uses eval STRING, and most of its replacement are either XS or use
 # eval STRING as well
 
+# Since we're not going to have eval STRING, we could attempt to do the
+# following for a nice speed boost due to the OO heavy nature of this
+# code:
+# eval {
+#     use Class::XSAccessor::Compat;
+#     our @ISA = ('Class::XSAccessor::Compat');
+#     1
+# } or do {
+#     *mk_ro_accessors = sub {...};
+#     *mk_wo_accessors = sub {...};
+#     *mk_accessors = sub {...};
+# }
+# Now, that module is XS, so it's not desireable for the self-parse test
+# and bootstrapping, but if we can have it, I don't see a reason why not.
+
 sub new {
     return bless { %{$_[1] || {}} }, ref $_[0] || $_[0];
 }
